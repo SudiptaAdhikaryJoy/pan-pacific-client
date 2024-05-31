b846ce4ab140a8599fefd443250d3dd7228321f2
SudiptaAdhikaryJoy <sudiptaadhikaryjoy@gmail.com>
2024-05-31 21:30:02 +0600

    refactor: header section optimizing and next ui navbar added

--- src/components/ui/features/Header/Header.tsx
+++ src/components/ui/features/Header/Header.tsx
@@ -3,11 +3,12 @@
 import Image from "next/image";
 import  logo  from '../../../../../public/assets/indentaImage.png'
 import Link from "next/link";
+import { Avatar, Dropdown, DropdownItem, DropdownMenu, DropdownTrigger, Input, Navbar, NavbarContent } from "@nextui-org/react";
 
 const Header = () => {
 
   return (
-    <div>
+    <Navbar isBordered>
        <div className=" grid grid-cols-3">
         <div className="flex justify-center items-center">
           <Image src={logo} alt="hotelLogo" width={100} height={100} />
@@ -17,11 +18,54 @@ const Header = () => {
           <Link href="/AboutUs">About</Link>
           <Link href="/services">Services</Link>
         </div>
-        <div className="flex justify-center items-center">
+        <div className="flex justify-center items-center gap-3">
           <h1>Login</h1>
+          <NavbarContent as="div" className="items-center" justify="end">
+        <Input
+          classNames={{
+            base: "max-w-full sm:max-w-[10rem] h-10",
+            mainWrapper: "h-full",
+            input: "text-small",
+            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+          }}
+          placeholder="Type to search..."
+          size="sm"
+          // startContent={<SearchIcon size={18} />}
+          type="search"
+        />
+        <Dropdown placement="bottom-end">
+          <DropdownTrigger>
+            <Avatar
+              isBordered
+              as="button"
+              className="transition-transform"
+              color="secondary"
+              name="Jason Hughes"
+              size="sm"
+              src="https://i.pravatar.cc/150?u=a042581f4e29026704d"
+            />
+          </DropdownTrigger>
+          <DropdownMenu aria-label="Profile Actions" variant="flat">
+            <DropdownItem key="profile" className="h-14 gap-2">
+              <p className="font-semibold">Signed in as</p>
+              <p className="font-semibold">zoey@example.com</p>
+            </DropdownItem>
+            <DropdownItem key="settings">My Settings</DropdownItem>
+            <DropdownItem key="team_settings">Team Settings</DropdownItem>
+            <DropdownItem key="analytics">Analytics</DropdownItem>
+            <DropdownItem key="system">System</DropdownItem>
+            <DropdownItem key="configurations">Configurations</DropdownItem>
+            <DropdownItem key="help_and_feedback">Help & Feedback</DropdownItem>
+            <DropdownItem key="logout" color="danger">
+              Log Out
+            </DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+      </NavbarContent>
         </div>
+        
       </div>      
-    </div>
+    </Navbar>
   )
 }
 
